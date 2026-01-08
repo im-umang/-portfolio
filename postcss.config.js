08da1816be6f7c8e6d40d9c4765e85ce73a090f1
Umang Trivedi <utrivedi180@gmail.com>
2026-01-08 20:33:21 +0530

    chore: Resolve merge conflicts and update build configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> 4649de81 (feat: Upgrade portfolio to Extreme level with dark theme and glassmorphism)
